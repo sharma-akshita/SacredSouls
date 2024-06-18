686f67428bdd2911a0d13d70e4c97fc11c93583b
Akshita Sharma <151676076+sharma-akshita@users.noreply.github.com>
2024-06-18 15:11:51 -0400

    Updated Code with Form Working

---
+++ php/contact_form.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "radiante_akshita";
+$password = "]ThQDL_RKTdU";
+$dbname = "radiante_sacredsouls";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully"; // Add this line for debugging
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $conn->real_escape_string($_POST['name']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $phone = $conn->real_escape_string($_POST['phone']);
+    $address = $conn->real_escape_string($_POST['address']);
+    $subject = $conn->real_escape_string($_POST['subject']);
+    $message = $conn->real_escape_string($_POST['message']);
+
+    $sql = "INSERT INTO contact (Name, Email, Phone, Address, Subject, Message) VALUES ('$name', '$email', '$phone', '$address', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
